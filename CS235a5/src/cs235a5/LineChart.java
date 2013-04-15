957747a9cbd8bc31ad58f53b4d94acfb287f84e5
DWJones <635773@swansea.ac.uk>
2013-04-15 15:59:46 +0200

    Update LineChart.java
    Doxygen and commenting complete on class

--- CS235a5/src/cs235a5/LineChart.java
+++ CS235a5/src/cs235a5/LineChart.java
@@ -3,74 +3,100 @@ package cs235a5;
 /**
  *
  * \file        LineChart.java
- * \author      Robert Fletcher
+ * \author      William Jones
  * 
- * \date        11/04/2013
+ * \date        15/04/2013
  *
  * \brief       The Area Chart Class generates and displays an
  *              Area chart by inheriting from the Chart class.
  *
  */
 
-// Import JFreeChart
+// Import JFreeChart and utilities 
 import org.jfree.chart.*;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.chart.renderer.category.*;
-import java.awt.Rectangle;
-
-import java.awt.Paint;
-import java.util.ArrayList;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import java.awt.Rectangle;
+import java.awt.Paint;
+import java.util.ArrayList;
 
-public class LineChart extends Chart {
+
+public class LineChart extends Chart{
  
   private JFreeChart m_chart;
   
   
-    
+  /**
+  * Returns the data set  
+  * @return true the set data is correct;
+  */  
   @Override
   public boolean SetDataSet(DataSet data){
       super.SetData(data);
       return true;
   }
   
+  /**
+  * returns the x axis values
+  * @return xData set values;
+  */ 
   @Override
   public boolean SetXData(int xData){
       super.SetXData(xData);
       return true;
   }
   
+  /**
+  * returns the y axis values
+  * @return yData set values;
+  */
   @Override
   public boolean SetYData(int yData){
       super.SetYData(yData);
       return true;
   }
-  
+  /**
+  * returns the title of the Chart
+  * @return Chart title of the Chart;
+  */
   @Override
   public boolean SetChartTitle(String title){
       super.SetChartTitle(title);
       return true;
   }
   
+  /**
+  * Returns the colourmap selected 
+  * @return colourmap set of the Chart;
+  */
   @Override
   public boolean SetColourMap(ColourMap colours){
       super.SetColourMap(colours);
       return true;
   }
   
+  /**
+  * Returns a aurthor
+  * @return author of the Chart;
+  */
   @Override
   public boolean SetAuthor(String author){
       super.SetAuthor(author);
       return true;
   }
   
+  /**
+  * returns the description
+  * @return description of the Chart class;
+  */
   @Override
   public boolean SetDescription(String description){
       super.SetDescription(description);
@@ -78,43 +104,41 @@ public class LineChart extends Chart {
   }
   
   /**
-   * Sets the type of chart to the constant AreaChart
-   * @return true of set correctly;
-   */
+  * Sets the type of chart to the constant AreaChart
+  * @return true of set correctly;
+  */
   public boolean SetChartType(){
       return super.SetChartType(ChartType.LINECHART);
   }
   
   /**
-   * returns the chart type
-   * @return the constant AREACHART
-   */
+  * returns the chart type
+  * @return the constant AREACHART
+  */
   public ChartType getChartType(){
       return super.GetChartType();
   }
   
   /**
-   * Returns the X Axis Column Position
-   * @return Int X Axis Column Position
-   */
-  public int GetXData()
-  {
+  * Returns the X Axis Column Position
+  * @return Int X Axis Column Position
+  */
+  public int GetXData(){
     return super.GetXColumnPosition();
   }
 
   /**
-   * Returns the Y Values for the Column
-   * @return m_yValues The Y Axis Data
-   */
-  public int GetYData()
-  {
+  * Returns the Y Values for the Column
+  * @return m_yValues The Y Axis Data
+  */
+  public int GetYData(){
     return super.GetYColumnPosition();
   }
 
   /**
-   * Returns the Description for the Chart
-   * @return m_Description a description of the char thats been genarated
-   */
+  * Returns the Description for the Chart
+  * @return m_Description a description of the char thats been genarated
+  */
   @Override
   public String GetDescription(){
 
@@ -122,12 +146,11 @@ public class LineChart extends Chart {
   }
 
   /**
-   * Returns the name of the Chart
-   * @return m_chartName the name of the chart
-   */
+  * Returns the name of the Chart
+  * @return m_chartName the name of the chart
+  */
   @Override
-  public String GetTitle()
-  {
+  public String GetTitle(){
     return super.GetChartTitle();
   }
 
@@ -148,15 +171,14 @@ public class LineChart extends Chart {
       
   }
   
-  /**
+   /**
    * Creates a dataset of type XYSeries
    */
-   public XYDataset convertDataSet()
-  {
-        int size = 0;
-        int sum = 0;
-        int pos = 0;
-        int j= 0;                                                    //.........needs to be changed!?!?........
+   public XYDataset convertDataSet(){
+        int size = 0;                                                //Initialises a value for size
+        int sum = 0;                                                 //initialises a value for sum
+        int pos = 0;                                                 //initialises a value for position
+        int j= 0;                                                    //initialises a increment for the for loop 
         
         XYSeriesCollection dataset = new XYSeriesCollection();
         XYSeries series = new XYSeries(super.GetTitle());
@@ -183,7 +205,6 @@ public class LineChart extends Chart {
                 super.m_foundElements.add(super.GetDataSet().GetCell(super.GetXColumnPosition(), i).toString());
 
                 //Add to chart dataSet
-
                  series.add(sum,super.GetDataSet().GetCell(
                         super.GetXColumnPosition(), (i)).GetInteger());
 
@@ -200,21 +221,21 @@ public class LineChart extends Chart {
    
 
    /**
-    *
-    * Create the column chart using the ChartFactory built into JFreeChart
-    * @return m_chart A chart of type JFreeChart
-    */
-   @Override                                                 //.....needs to be changed?!?!................
+   *
+   * Create the column chart using the ChartFactory built into JFreeChart
+   * @return m_chart A chart of type JFreeChart
+   */
+   @Override                                                 
     public JFreeChart createChart(){
         final JFreeChart CHART = ChartFactory.createXYLineChart(
-            super.GetTitle(),         // chart title
+            super.GetTitle(),                                                            // chart title
             super.GetDataSet().GetAColumnName(super.GetXColumnPosition()),               // domain axis label
-            super.GetDataSet().GetAColumnName(super.GetYColumnPosition()),                  // range axis label
+            super.GetDataSet().GetAColumnName(super.GetYColumnPosition()),               // range axis label
             convertDataSet(),            
-            PlotOrientation.VERTICAL, // orientation
-            true,                     // include legend
-            true,                     // tooltips?
-            false                     // URLs?
+            PlotOrientation.VERTICAL,                                                    // orientation
+            true,                                                                        // include legend
+            true,                                                                        // tooltips?
+            false                                                                        // URLs?
         );
         
         final XYPlot plot = (XYPlot) CHART.getPlot();
@@ -225,17 +246,17 @@ public class LineChart extends Chart {
    
    
    
-   /**
-     * A renderer specific for this type of chart. Sets the colours that will 
-     * be used when displaying the chart.
-     */
-    class CustomRenderer extends XYLineAndShapeRenderer {
+    /**
+    * A renderer specific for this type of chart. Sets the colours that will 
+    * be used when displaying the chart.
+    */
+    class CustomRenderer extends XYLineAndShapeRenderer{
         private Paint[] colors;
         
         /**
-         * The constructor for the custom renderer, which will set the colours
-         * of the Area
-         */
+        * The constructor for the custom renderer, which will set the colours
+        * of the Area
+        */
         public CustomRenderer(){ 
            ColourMap mappedColours = GetColourMap();
           this.colors = new Paint[mappedColours.getColourArray().length];
@@ -245,22 +266,22 @@ public class LineChart extends Chart {
         }
         
         /**
-         * Accessor method for getting the colours of the area
-         * @param row - the identifier of the row it is returning
-         * @param column - the identifier of the area it is returning
-         * @return the colour of each area 
-         */
+        * Accessor method for getting the colours of the area
+        * @param row - the identifier of the row it is returning
+        * @param column - the identifier of the area it is returning
+        * @return the colour of each area 
+        */
         @Override
-        public Paint getItemPaint(final int row, final int column) {     //............needs to be changed?!.........
+        public Paint getItemPaint(final int row, final int column){
            return (this.colors[column % this.colors.length]); 
         } 
     }
+    
    /**
-    *  A method to return the chart created by this class so it can be outputted.
-    * @return m_chart the chart created by this class
-    */
-   public JFreeChart GetJChart()
-   {
+   *  A method to return the chart created by this class so it can be outputted.
+   * @return m_chart the chart created by this class
+   */
+   public JFreeChart GetJChart(){
      return m_chart;
    }
 }
