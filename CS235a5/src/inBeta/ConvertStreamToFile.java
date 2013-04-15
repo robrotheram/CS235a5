2592b35c93a182cf6f6d9b1fd3d644e0556eae7e
robrotheram <robrotheram@gmail.com>
2013-04-16 01:49:09 +0200

    Create ConvertStreamToFile.java

---
+++ CS235a5/src/inBeta/ConvertStreamToFile.java
@@ -0,0 +1,22 @@
+ /**
+     * Private method to get the file inside of the jar file
+     * @return File
+     */ 
+    private File getFile(){
+        String  p = System.getProperty("user.dir")+"/rrf.csv";
+        try{
+            InputStream is = is = this.getClass().getResourceAsStream("/assets/files/csv.csv");
+            OutputStream stream = new BufferedOutputStream(new FileOutputStream(p));
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            int len = 0;
+            while ((len = is.read(buffer)) != -1) {
+                stream.write(buffer, 0, len);
+            }
+            if(stream!=null){
+                stream.close();
+            }
+        }catch(IOException e){}
+        return new File(p);
+    }
+
