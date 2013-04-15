6b90a96e510805aa247366823048fd427d205d9f
DWJones <635773@swansea.ac.uk>
2013-04-15 21:08:18 +0200

    Update BubbleChart.java

--- CS235a5/src/cs235a5/BubbleChart.java
+++ CS235a5/src/cs235a5/BubbleChart.java
@@ -29,102 +29,149 @@ public class BubbleChart extends Chart{
    
   private JFreeChart m_chart;
 
+   /**Allows access for setting the dataset
+  * Sets the data set to be used to produce a graph
+  *
+  * @param data dataset to be set
+  * @return returns true is set if successful
+  */
   @Override
   public boolean SetDataSet(DataSet data){
       super.SetData(data);
       return true;
   }
   
+  /**Sets access for the x axis data 
+   * Allows access for setting the x axis data to be used for making charts
+   *
+   * @param xData
+   * @return
+   */
   @Override
   public boolean SetXData(int xData){
       super.SetXData(xData);
       return true;
   }
   
+  /**Sets access for the y axis data
+   * Allows access for setting the y axis data to be used for making charts
+   *
+   * @param xData
+   * @return
+   */
   @Override
   public boolean SetYData(int yData){
       super.SetYData(yData);
       return true;
   }
   
+  /** Sets the title of the Chart
+   * Alllows access for setting a title to be used with the chart
+   *
+   * @param title
+   * @return Chart title of the Chart;
+   */
   @Override
   public boolean SetChartTitle(String title){
       super.SetChartTitle(title);
       return true;
   }
   
+  /**Returns the colourmap selected
+   * Sets the colour to be applied to the chart
+   *
+   * @param colours
+   * @return colourmap set of the Chart;
+   */
   @Override
   public boolean SetColourMap(ColourMap colours){
       super.SetColourMap(colours);
       return true;
   }
   
+  /**Returns an author
+   * sets the author's name to be used on the chart
+   *
+   * @param author
+   * @return author of the Chart;
+   */
   @Override
   public boolean SetAuthor(String author){
       super.SetAuthor(author);
       return true;
   }
   
+  /**sets the description
+   * sets a description to inform the user what the chart represents
+   *
+   * @param description
+   * @return description of the Chart class;
+   */
   @Override
   public boolean SetDescription(String description){
       super.SetDescription(description);
       return true;
   }
   
-  /**
-   * Sets the type of chart to the constant AreaChart
+  /**Sets the Chart type
+   * Sets the type of chart to the constant BubbleChart
+   * 
    * @return true of set correctly;
    */
   public boolean SetChartType(){
       return super.SetChartType(ChartType.BUBBLECHART);
   }
   
-  /**
-   * returns the chart type
-   * @return the constant BUBBLECHART
+  /** Returns the chert type
+   * returns what kind of chart is being created
+   * 
+   * @return the constant LINECHART
    */
   public ChartType getChartType(){
       return super.GetChartType();
   }
   
-  /**
-   * Returns the X Axis Column Position
+  /**Returns the X Axis Column Position
+   * Returns the X axis column position of data to be 
+   * used on the graph
+   * 
    * @return Int X Axis Column Position
    */
-  public int GetXData()
-  {
+  public int GetXData(){
     return super.GetXColumnPosition();
   }
 
-  /**
-   * Returns the Y Values for the Column
+  /**Returns the Y Values for the Column
+   * Returns the Y axis column position of data to be 
+   * used on the graph
+   * 
    * @return m_yValues The Y Axis Data
    */
-  public int GetYData()
-  {
+  public int GetYData(){
     return super.GetYColumnPosition();
   }
 
-  /**
-   * Returns the Description for the Chart
+  /**Returns the Description for the Chart
+   * Returns the description of the what the chart represents
+   * 
    * @return m_Description a description of the char thats been generated
    */
   @Override
   public String GetDescription(){
-
     return super.GetDescription();
   }
 
-  /**
-   * Returns the name of the Chart
+  /**Returns the name of the Chart
+   * returns the chart title to identify what the chart is called
    * @return m_chartName the name of the chart
    */
   @Override
-  public String GetTitle()
-  {
+  public String GetTitle(){
     return super.GetChartTitle();
   }
-    /**
+  
+  
+    /**Creates class variables for a bubble chart
      * constructor setting all class variables needed to create a bubble chart
      * @param ds - the data for the chart
      * @param xColPosition - the column data for the x axis from the dataset
@@ -150,10 +197,10 @@ public class BubbleChart extends Chart{
         ArrayList<Integer>yVal = new ArrayList<Integer>();
         ArrayList<Double>sumVal = new ArrayList<Double>();
         
-        int size = 0;
-        int sum = 0;
-        int pos = 0;
-        int j= 0;                                                    //.........needs to be changed!?!?........
+        int size = 0;                            //Initalises the size variable
+        int sum = 0;                             //Initalises the total number variable
+        int pos = 0;                             //Initalises the position variable
+        int j= 0;                                //Initalises the increment value for the for loop                   
         
 
 
@@ -217,12 +264,12 @@ public class BubbleChart extends Chart{
      
      
     
-        /**
-    *
+   /**Creates a Bubble Chart
     * Create the bubble chart using the ChartFactory built into JFreeChart
+    * 
     * @return m_chart A chart of type JFreeChart
     */
-   @Override                                                 //.....needs to be changed?!?!................
+   @Override                                               
     public JFreeChart createChart(){
         final JFreeChart CHART = ChartFactory.createBubbleChart(
             super.GetTitle(),         // chart title
@@ -264,7 +311,7 @@ public class BubbleChart extends Chart{
     class CustomRenderer extends XYBubbleRenderer{
         private Paint[] colors;
         
-        /**
+        /** Custome renderer
          * The constructor for the custom renderer, which will set the colours
          * of the Area
          */
@@ -276,14 +323,14 @@ public class BubbleChart extends Chart{
            }
         }
         
-        /**
+        /** Gets the colours of an area
          * Accessory method for getting the colours of the area
          * @param row - the identifier of the row it is returning
          * @param column - the identifier of the area it is returning
          * @return the colour of each area 
          */
         
-        public Paint getItemPaint(final int row, final int column) {     //............needs to be changed?!.........
+        public Paint getItemPaint(final int row, final int column) {     
            return (this.colors[column % this.colors.length]); 
         } 
     }
